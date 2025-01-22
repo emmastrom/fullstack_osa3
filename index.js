818543a87caadf675c2a904d2ab3373112b3a0bf
Emma Lundström <1emma.lundstrom@gmail.com>
2025-01-22 23:43:03 +0200

    3.9

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 const morgan = require('morgan')
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
