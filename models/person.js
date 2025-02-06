fbe983c8cc85077421b5545eccfcaf8443cefe90
Emma Lundström <1emma.lundstrom@gmail.com>
2025-02-06 15:42:30 +0200

    3.20

--- models/person.js
+++ models/person.js
@@ -22,7 +22,16 @@ const personSchema = new mongoose.Schema({
     minlength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+        validator: function(v) {
+            return /^\d{2,3}-\d{6,}/.test(v)
+        },
+    },
+    required: true
+}
 })
 
 personSchema.set('toJSON', {
